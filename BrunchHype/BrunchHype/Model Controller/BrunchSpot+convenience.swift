5c789f7b29a0b5a243d03b1131b5c26d0e6b4a6f
Bryce Bradshaw <brycebradshaw@Bryces-MacBook-Pro.local>
2020-04-23 18:53:35 -0600

    Created Model

--- BrunchHype/BrunchHype/Model Controller/BrunchSpot+convenience.swift
+++ BrunchHype/BrunchHype/Model Controller/BrunchSpot+convenience.swift
@@ -5,5 +5,15 @@
 //  Created by Bryce Bradshaw on 4/23/20.
 //  Copyright © 2020 Karl Pfister. All rights reserved.
 //
-
 import Foundation
+import CoreData
+
+extension BrunchSpot {
+    
+    convenience init(name: String, tier: String = "Unrated", summary: String = "No summary added", context: NSManagedObjectContext = CoreDataStack.context) {
+        self.init(context:context)
+        self.name = name
+        self.tier = tier
+        self.summary = summary
+    }
+}
